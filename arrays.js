ef923d53439eb60d2025a1a375f8f232f65d69f4
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2019-04-09 02:59:36 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+ var newArray = [...array, element]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
+
+function accessElementInArray(array, index) {
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
+
+function removeElementFromBeginningOfArray(array) {
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
